0d1dfc2523dd534ce380e47abdd5ab95a896726d
duanxuzhe <626264312@qq.com>
2020-06-04 23:07:30 +0800

    four

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -11,8 +11,8 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="12">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -28,7 +28,7 @@
         <el-table-column prop="role_name" label="角色"></el-table-column>
         <el-table-column label="状态">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state"></el-switch>
+            <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
@@ -45,15 +45,15 @@
         </el-table-column>
       </el-table>
       <!-- 分页区域-->
-      <!-- <el-pagination
+      <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[100, 200, 300, 400]"
-        :page-size="100"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="400"
-      ></el-pagination> -->
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -65,8 +65,10 @@ export default {
       //获取用户列表的参数对象
       queryInfo: {
         query: "",
+      // 当前的页数
         pagenum: 1,
-        pagesize: 2
+      //当前每页显示多少条数据
+        pagesize: 5
       },
       userlist: [],
       total: 0
@@ -86,6 +88,31 @@ export default {
       this.userlist = res.data.users;
       this.total = res.data.total;
       console.log(res);
+    },
+    //监听pagesize改变的事件
+    handleSizeChange(newSize){
+      console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    //监听页码值改变的事件
+    handleCurrentChange(newPage){
+      console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+    //监听switch开关状态的改变
+    async userStateChanged(userinfo){
+      console.log(userinfo)
+      const { data:res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      )
+      if (res.meta.status !== 200){
+        userinfo.mg_state = !userinfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      }
+      this.$message.success("更新用户状态成功！")
+
     }
   }
 };
